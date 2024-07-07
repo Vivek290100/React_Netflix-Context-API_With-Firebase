cac4e4472d21b370460858c119d0c746235f82d5
vivek <vivekv290100@gmail.com>
2024-07-07 20:42:12 +0530

    finished

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from 'react';
+import SavedShows from '../components/savedShows';
 
 const Account = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <>
+      <div className='w-full text-white'>
+      <img
+        className="w-full h-[400px] object-cover"
+        src="https://assets.nflxext.com/ffe/siteui/vlv3/0552717c-9d8c-47bd-9640-4f4efa2de663/537e2c5e-c750-4d4c-9f7a-e66fe93eb977/IN-en-20240701-POP_SIGNUP_TWO_WEEKS-perspective_WEB_b00eeb83-a7e8-4b5b-8ff7-86ed92c51caf_large.jpg"
+        alt="/"
+      />
+        <div className='bg-black/60 fixed top-0 left-0 w-full h-[550px]'></div>
+        <div className='absolute top-[20%] p-4 md:p-8'>
+          <h1 className='text-3xl md:text-5xl font-bold'>My Shows</h1>
+        </div>
+      </div>
+      <SavedShows />
+    </>
+  );
+};
 
-export default Account
+export default Account;
